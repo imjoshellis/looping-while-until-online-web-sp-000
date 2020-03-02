0e3a9c0d20b249884b33ff00b072b56c43b37d61
Josh Ellis <joshe523@gmail.com>
2020-03-02 03:44:15 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,4 +1,4 @@
-def using_until 
+def using_until
   levitation_force = 6
   #your code here
   until levitation_force == 10 do
@@ -6,4 +6,3 @@ def using_until
     levitation_force += 1
   end
 end
-
